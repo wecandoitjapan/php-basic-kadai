394a3a4d9a7014638a96724cf6d9fc842c164f51
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-01-09 22:33:08 +0900

    テストの平均点を計算しよう 初回

---
+++ kadai_005/score.php
@@ -0,0 +1,21 @@
+<?php
+$socre1 = 80;
+$socre2 = 60;
+$socre3 = 55;
+$socre4 = 40;
+$socre5 = 100;
+$socre6 = 25;
+$socre7 = 80;
+$socre8 = 95;
+$socre9 = 30;
+$socre10 = 60;
+
+$total =  $socre1 + $socre2 + $socre3 + $socre4 + $socre5 + $socre6 + $socre7 + $socre8 + $socre9 + $socre10;
+echo $total;
+
+echo'<br>';
+
+echo $total / 10;
+
+?>
+
