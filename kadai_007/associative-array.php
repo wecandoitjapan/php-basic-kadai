60751d96fb0212b4a76572def942444e8cc8e3b1
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-01-10 22:42:55 +0900

    連想配列を作ってキーと値を出力しよう 初回

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+    <meta chraset="UTF-8">
+    <title>課題 連想配列を作ってキーと値を出力しよう</title>
+</head>
+
+<body>
+    <p>
+    <?php
+$goods_inf = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+print_r($goods_inf);
+    ?>
+</P>
+</body>
+
+</html>
+
+
+
